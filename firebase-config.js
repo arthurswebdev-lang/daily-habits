c2916a7f886d5819f484a6b4d123eefce51deb31
Artur Sargsyan <sargsyan.artur92@gmail.com>
2026-08-04 20:42:35 +0400

    Add Firebase config for push notifications

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Cloud Messaging configuration for push notifications
+globalThis.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDLEtEVv1iuKWoRuN218XZ5-mSFHsk20XQ",
+  authDomain: "auth-test-ef465.firebaseapp.com",
+  databaseURL: "https://auth-test-ef465.firebaseio.com",
+  projectId: "auth-test-ef465",
+  storageBucket: "auth-test-ef465.firebasestorage.app",
+  messagingSenderId: "577996770911",
+  appId: "1:577996770911:web:8425b656dad06a664b15ea",
+  measurementId: "G-5J9M806D8C"
+};
+
+globalThis.FCM_VAPID_KEY = "BAWx51EaD7f9SE2lPr-Ra_Jaj0NX6o6urDUfTYs6hNeJhZlC8GML8U0iTKjcHbUrOZiMfnpO2bILCqXOem4EMEs";
